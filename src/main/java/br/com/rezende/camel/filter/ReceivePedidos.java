a754deffb7850d97a5bd4be93b5dca6eb08ef2b3
Andre Rezende <meirezende@hotmail.com>
2016-05-21 10:12:44 -0300

    Added filter using XPATH

--- src/main/java/br/com/rezende/camel/filter/ReceivePedidos.java
+++ src/main/java/br/com/rezende/camel/filter/ReceivePedidos.java
@@ -4,6 +4,11 @@ import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
+/**
+ * 
+ * @author Rezende
+ *
+ */
 public class ReceivePedidos {
 
 	public static void main(String[] args) throws Exception {
@@ -14,18 +19,15 @@ public class ReceivePedidos {
 			@Override
 			public void configure() throws Exception {
 
-				from("file:pedidos?noop=true&delay=5s").
-					log("${id}").
-					marshal().xmljson().
-					log("${body}").
-					setHeader("CamelFileName", simple("${file:name.noext}.json")).
-				to("file:saida");
+				from("file:pedidos?noop=true&delay=5s").log("${id}").split().xpath("/pedido/itens/item").filter()
+						.xpath("/item/formato[text()='PDF']").marshal().xmljson().log("${body}")
+						.setHeader("CamelFileName", simple("${file:name.noext}.json")).to("file:saida");
 			}
-			
+
 		});
 
 		context.start();
 		Thread.sleep(20000);
 		context.stop();
-	}	
+	}
 }
