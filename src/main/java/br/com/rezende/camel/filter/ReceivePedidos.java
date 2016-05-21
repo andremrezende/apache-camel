c98825e6427ee7416f367e2e1ee0c82cfb75c932
Andre Rezende <meirezende@hotmail.com>
2016-05-21 09:57:28 -0300

    Apache camel input and output orders.

---
+++ src/main/java/br/com/rezende/camel/filter/ReceivePedidos.java
@@ -0,0 +1,31 @@
+package br.com.rezende.camel.filter;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class ReceivePedidos {
+
+	public static void main(String[] args) throws Exception {
+
+		CamelContext context = new DefaultCamelContext();
+		context.addRoutes(new RouteBuilder() {
+
+			@Override
+			public void configure() throws Exception {
+
+				from("file:pedidos?noop=true&delay=5s").
+					log("${id}").
+					marshal().xmljson().
+					log("${body}").
+					setHeader("CamelFileName", simple("${file:name.noext}.json")).
+				to("file:saida");
+			}
+			
+		});
+
+		context.start();
+		Thread.sleep(20000);
+		context.stop();
+	}	
+}
