9820702d34381752a7fef606b3f4e74d0086bb2b
Andre Rezende <meirezende@hotmail.com>
2016-05-22 09:42:20 -0300

    Changed output file name.

--- src/main/java/br/com/rezende/camel/filter/ReceivePedidosFilter.java
+++ src/main/java/br/com/rezende/camel/filter/ReceivePedidosFilter.java
@@ -1,12 +1,13 @@
 package br.com.rezende.camel.filter;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
 /**
  * Class to receive book orders and filter PDF books, transforming XML files to
- * JSON on output folder saida.
+ * JSON on output folder saida. The whole example uses Enterprise Integration Patterns.
  * 
  * @author Andre Rezende
  *
@@ -19,16 +20,15 @@ public class ReceivePedidosFilter {
 
 			@Override
 			public void configure() throws Exception {
-
 				from("file:pedidos?noop=true&delay=5s").log("${id}").split().xpath("/pedido/itens/item").filter()
 						.xpath("/item/formato[text()='PDF']").marshal().xmljson().log("${body}")
-						.setHeader("CamelFileName", simple("${file:name.noext}.json")).to("file:saida");
+						.setHeader(Exchange.FILE_NAME, simple("${file:name.noext}-${header.CamelSplitIndex}.json")).to("file:saida");
 			}
 
 		});
 
 		context.start();
-		Thread.sleep(20000);
+		Thread.sleep(2000);
 		context.stop();
 	}
 }
